0928cdbe81d6cf4dfa6fc306ccea4b2b298327ad
Brian Kemper <brian.kemper@sparkpost.com>
2018-08-12 10:51:28 -0400

    Set title prop on icons for accessibility

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import * as icons from 'react-feather';
-import styled from 'styled-components';
-import { OutboundLink } from 'gatsby-plugin-google-analytics';
-import * as colors from '../constants/colors';
+import React from 'react'
+import * as icons from 'react-feather'
+import styled from 'styled-components'
+import { OutboundLink } from 'gatsby-plugin-google-analytics'
+import * as colors from '../constants/colors'
 
 const Anchor = styled(OutboundLink)`
   color: ${({ color }) => colors[color.toUpperCase()]};
@@ -12,16 +12,19 @@ const Anchor = styled(OutboundLink)`
     color: ${colors.BLUE};
     transition: color 0.8s;
   }
-`;
+`
 
 const Link = ({ color = 'grey', href, icon, text }) => {
-  const Icon = icons[icon];
+  const Icon = icons[icon]
 
+  // Must set title of icon for accessiblity
+  // SEE: https://www.sitepoint.com/15-rules-making-accessible-links/
+  // SEE: https://css-tricks.com/accessible-svgs/
   return (
     <Anchor color={color} href={href}>
-      {Icon ? <Icon size={20} /> : text}
+      {Icon ? <Icon size={20} title={icon} /> : text}
     </Anchor>
-  );
+  )
 }
 
-export default Link;
+export default Link
