e0886d057f13438d754324c3364b92972793d5bf
Brian Kemper <brian.kemper@sparkpost.com>
2018-08-12 10:13:48 -0400

    PWA, yay!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,26 @@
+const colors = require('./src/constants/colors');
+// console.log(colors);
+
 module.exports = {
   siteMetadata: {
     // title: 'Gatsby Default Starter',
   },
   plugins: [
+    // SEE: https://next.gatsbyjs.org/tutorial/part-eight/#-using-gatsby-plugin-manifest
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Brian Patrick Kemper",
+        short_name: "Brian Kemper",
+        start_url: "/",
+        background_color: colors.DARK_BLUE,
+        theme_color: colors.DARK_BLUE,
+        display: "minimal-ui",
+        icon: "src/images/logo.png"
+      }
+    },
+    // SEE: https://next.gatsbyjs.org/tutorial/part-eight/#-using-gatsby-plugin-offline
+    'gatsby-plugin-offline',
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     {
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -1,8 +1,10 @@
-export const BLUE = '#4FD7D8';
-export const DARK_BLUE = '#1c2131'; // A
-export const LIGHT_BLUE = '#a4d8e6';  // B
-export const GREEN = '#1FD6AF';
-export const GREY = '#47537c';
-export const ORANGE = '#FA6423';
-export const PURPLE = '#7468ED';
-export const WHITE = '#FFFFFF';
+module.exports = {
+  BLUE: '#4FD7D8',
+  DARK_BLUE: '#1c2131', // A
+  LIGHT_BLUE: '#a4d8e6',  // B
+  GREEN: '#1FD6AF',
+  GREY: '#47537c',
+  ORANGE: '#FA6423',
+  PURPLE: '#7468ED',
+  WHITE: '#FFFFFF'
+}
