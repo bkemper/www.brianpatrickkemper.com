2873173c9b4cea1fb0a6b86606136e49b2e966ea
Brian Kemper <brian.kemper@sparkpost.com>
2022-04-05 10:45:17 -0400

    Added react-gtm-module

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app';
 import Script from 'next/script';
 import { ChakraProvider } from '@chakra-ui/react';
+import TagManager from 'react-gtm-module';
 import theme from '../theme';
 
 // yuck!
@@ -23,29 +24,17 @@ import '@fontsource/raleway/800-italic.css';
 import '@fontsource/raleway/900.css';
 import '@fontsource/raleway/900-italic.css';
 
+const gtmId = process.env.GOOGLE_TAG_MANAGER_ID;
+
+if (typeof window !== 'undefined' && gtmId) {
+  TagManager.initialize({ gtmId });
+}
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
-      </ChakraProvider>
-      {process.env.GOOGLE_TAG_MANAGER_ID && (
-        <>
-          <Script
-            id="google-tag-manager"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${process.env.GOOGLE_TAG_MANAGER_ID}');`,
-            }}
-          />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.GOOGLE_TAG_MANAGER_ID}"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>`,
-            }}
-          />
-        </>
-      )}
-    </>
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
   );
 }
 
