3d07f8088f727626d356146d42722f45fd6be191
Brian Kemper <Kemper10@gmail.com>
2025-04-09 09:00:10 -0400

    fix OfflineOverlay opacity

--- components/OfflineOverlay.tsx
+++ components/OfflineOverlay.tsx
@@ -10,9 +10,9 @@ const OfflineOverlay = () => {
   return (
     <Dialog.Root open={isWindowOnline === false}>
       <Dialog.Portal>
-        <Dialog.Overlay className="animate-[fadeIn_500ms_ease-in-out_1] backdrop-blur-[2px] bg-opacity-75 bg-day dark:bg-opacity-75 dark:bg-night fixed inset-0" />
+        <Dialog.Overlay className="animate-[fadeIn_500ms_ease-in-out_1] backdrop-blur-[2px] bg-day/75 dark:bg-night/75 fixed inset-0" />
         <Dialog.Content className="animate-[fadeIn_1s_ease-in-out_1] fixed max-w-md p-6 text-night dark:text-day top-0">
-          <Dialog.Title className="text-lg">Lost Connection</Dialog.Title>
+          <Dialog.Title className="mb-1 text-2xl">Lost Connection</Dialog.Title>
           <Dialog.Description className="text-sm">
             It is a bummer that you lost your internet connection. Try shaking your mouse, yelling
             at your internet service provider, or restart your computer 3 times.
