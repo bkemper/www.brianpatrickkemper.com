8613e25a2025d64b05dda3c5cb07c3e3a8a37f14
Brian Kemper <Kemper10@gmail.com>
2025-04-09 08:58:50 -0400

    exclude Pulumi resources from dev

--- sst.config.ts
+++ sst.config.ts
@@ -24,6 +24,9 @@ export default $config({
 
     const service = new sst.aws.Service("BpkService", {
       cluster: new sst.aws.Cluster("BpkCluster", { vpc }),
+      dev: {
+        command: "npx next dev --port 3000",
+      },
       image: {
         dockerfile: "Dockerfile",
       },
@@ -33,81 +36,85 @@ export default $config({
       },
     });
 
-    const origin = new aws.cloudfront.VpcOrigin("BpkVpcOrigin", {
-      vpcOriginEndpointConfig: {
-        arn: service.nodes.loadBalancer.arn,
-        httpPort: 80,
-        httpsPort: 443,
-        name: "BpkVpcOrigin",
-        originProtocolPolicy: "http-only",
-        originSslProtocols: {
-          items: ["TLSv1.2"],
-          quantity: 1,
-        },
-      },
-    });
-
-    const cache = new aws.cloudfront.CachePolicy("BpkCachePolicy", {
-      defaultTtl: 3600,
-      maxTtl: 86400,
-      minTtl: 0,
-      name: "BpkCachePolicy",
-      parametersInCacheKeyAndForwardedToOrigin: {
-        cookiesConfig: {
-          cookieBehavior: "none",
-        },
-        headersConfig: {
-          headerBehavior: "none",
-        },
-        queryStringsConfig: {
-          queryStringBehavior: "none",
-        },
-      },
-    });
-
-    const cert = new aws.acm.Certificate("BpkCertificate", {
-      domainName: "brianpatrickkemper.com",
-      subjectAlternativeNames: ["www.brianpatrickkemper.com"],
-      validationMethod: "DNS",
-    });
-
-    new aws.cloudfront.Distribution("BpkCdn", {
-      // note, must update the cert and verify before changing aliases
-      aliases: $resolve(cert.subjectAlternativeNames).apply((subjectAlternativeNames) => [
-        cert.domainName,
-        ...subjectAlternativeNames,
-      ]),
-      defaultCacheBehavior: {
-        allowedMethods: ["GET", "HEAD", "OPTIONS"],
-        cachedMethods: ["GET", "HEAD"],
-        cachePolicyId: cache.id,
-        compress: true,
-        targetOriginId: "BpkVpcOrigin",
-        viewerProtocolPolicy: "redirect-to-https",
-      },
-      enabled: true,
-      origins: [
-        {
-          domainName: service.nodes.loadBalancer.dnsName,
-          originId: "BpkVpcOrigin",
-          vpcOriginConfig: {
-            vpcOriginId: origin.id,
+    if ($dev) {
+      //
+    } else {
+      const origin = new aws.cloudfront.VpcOrigin("BpkVpcOrigin", {
+        vpcOriginEndpointConfig: {
+          arn: service.nodes.loadBalancer.arn,
+          httpPort: 80,
+          httpsPort: 443,
+          name: "BpkVpcOrigin",
+          originProtocolPolicy: "http-only",
+          originSslProtocols: {
+            items: ["TLSv1.2"],
+            quantity: 1,
           },
         },
-      ],
-      priceClass: "PriceClass_200",
-      restrictions: {
-        geoRestriction: {
-          restrictionType: "none",
+      });
+
+      const cache = new aws.cloudfront.CachePolicy("BpkCachePolicy", {
+        defaultTtl: 3600,
+        maxTtl: 86400,
+        minTtl: 0,
+        name: "BpkCachePolicy",
+        parametersInCacheKeyAndForwardedToOrigin: {
+          cookiesConfig: {
+            cookieBehavior: "none",
+          },
+          headersConfig: {
+            headerBehavior: "none",
+          },
+          queryStringsConfig: {
+            queryStringBehavior: "none",
+          },
         },
-      },
-      staging: false,
-      viewerCertificate: {
-        acmCertificateArn: cert.arn,
-        cloudfrontDefaultCertificate: false,
-        minimumProtocolVersion: "TLSv1.2_2021",
-        sslSupportMethod: "sni-only",
-      },
-    });
+      });
+
+      const cert = new aws.acm.Certificate("BpkCertificate", {
+        domainName: "brianpatrickkemper.com",
+        subjectAlternativeNames: ["www.brianpatrickkemper.com"],
+        validationMethod: "DNS",
+      });
+
+      new aws.cloudfront.Distribution("BpkCdn", {
+        // note, must update the cert and verify before changing aliases
+        aliases: $resolve(cert.subjectAlternativeNames).apply((subjectAlternativeNames) => [
+          cert.domainName,
+          ...subjectAlternativeNames,
+        ]),
+        defaultCacheBehavior: {
+          allowedMethods: ["GET", "HEAD", "OPTIONS"],
+          cachedMethods: ["GET", "HEAD"],
+          cachePolicyId: cache.id,
+          compress: true,
+          targetOriginId: "BpkVpcOrigin",
+          viewerProtocolPolicy: "redirect-to-https",
+        },
+        enabled: true,
+        origins: [
+          {
+            domainName: service.nodes.loadBalancer.dnsName,
+            originId: "BpkVpcOrigin",
+            vpcOriginConfig: {
+              vpcOriginId: origin.id,
+            },
+          },
+        ],
+        priceClass: "PriceClass_200",
+        restrictions: {
+          geoRestriction: {
+            restrictionType: "none",
+          },
+        },
+        staging: false,
+        viewerCertificate: {
+          acmCertificateArn: cert.arn,
+          cloudfrontDefaultCertificate: false,
+          minimumProtocolVersion: "TLSv1.2_2021",
+          sslSupportMethod: "sni-only",
+        },
+      });
+    }
   },
 });
