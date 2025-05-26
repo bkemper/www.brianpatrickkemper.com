7f0e0f044f44b0e3c2ad90d7bbb4925b007c2b08
Brian Kemper <bkemper@users.noreply.github.com>
2025-05-26 12:55:59 -0400

    Switch back to CDK (#27)

---
+++ infra/app.ts
@@ -0,0 +1,19 @@
+import * as cdk from "aws-cdk-lib";
+import { WebsiteStack } from "./stacks/website-stack";
+import { AwsSolutionsChecks } from "cdk-nag";
+
+const app = new cdk.App();
+
+// see, https://aws.amazon.com/blogs/devops/manage-application-security-and-compliance-with-the-aws-cloud-development-kit-and-cdk-nag/
+cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
+
+new WebsiteStack(app, "WebsiteStack", {
+  // load environment from AWS profile
+  // see, https://docs.aws.amazon.com/cdk/v2/guide/environments.html
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+app.synth();
